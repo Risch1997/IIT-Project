9619531e7eabf46d94ddacd74b94b2a84793788e
MichaelvMorrison <morrim5@rpi.edu>
2017-12-03 13:53:16 -0500

    Restyled buttons in index.php

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 		<div id="select">
 		<h1>Select an option</h1>
 		<div class="groupoption">
-			<button class="selectbutton"><a href="create.php">Create group</a><br></button>
+			<a href="create.php" class="selecthref"><button class="selectbutton">Create group</button></a>
 		</div>
 		<div class="groupoption">
-			<button class="selectbutton"><a href="join.php">Join group</a><br></button>
+			<a href="join.php" class="selecthref"><button class="selectbutton">Join group</button></a>
 		</div>
 		<div class="groupoption">
-			<button class="selectbutton"><a href="view.php">View group</a><br></button>
+			<a href="view.php" class="selecthref"><button class="selectbutton">View group</button></a>
 		</div>
 	</body>
 </html>
