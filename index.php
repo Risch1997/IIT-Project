69ef03a59cc41cc78d9d57a281b02fd7c6cb6fe8
MichaelvMorrison <morrim5@rpi.edu>
2017-12-03 13:27:53 -0500

    Copied home.php into index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php include('includes/top.php'); ?>
 
+<?php include('includes/top.php'); ?>
+<html>
+	<body>
+		<h1>Select an option</h1>
+		<div class="groupoption">
+			<a href="create.php">Create group</a><br/
+		</div>
+		<div class="groupoption">
+			<a href="join.php">Join group</a><br/
+		</div>
+		<div class="groupoption">
+			<a href="view.php">View group</a><br/
+		</div>
 	</body>
 </html>
