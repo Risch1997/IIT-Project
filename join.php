f58139f0eb88673e6edc3701497d17e663c061d0
zhangc13 <zhangc13@rpi.edu>
2017-12-03 12:36:44 -0500

    Added files

---
+++ join.php
@@ -0,0 +1,12 @@
+<?php include('includes/top.php'); ?>
+<html>
+	<body>
+		<label style="float:right"><a href="home.php">Home</a></label>
+		<div id="join">
+			<td><label for="join">Send an invitation</label></td>
+			<td><input type="text" name="join" placeholder="Enter provided code"></td>
+			<td><input type="submit" name="joingroup" value="Join!"></td>
+		</div>
+		
+	</body>
+</html>
