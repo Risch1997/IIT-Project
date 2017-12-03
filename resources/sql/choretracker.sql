4c4c3b0fdfcf7ee48ad62ae1756280935ff585e2
risch1997 <risch1997@aol.com>
2017-12-03 12:36:19 -0500

    changes to database

--- resources/sql/choretracker.sql
+++ resources/sql/choretracker.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.4.14
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2017 at 07:01 AM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.10
+-- Generation Time: Dec 03, 2017 at 06:34 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,8 +26,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `chores`
 --
 
-CREATE TABLE `chores` (
-  `choreID` int(11) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `chores` (
+  `choreID` int(11) unsigned NOT NULL,
   `choreName` varchar(50) NOT NULL,
   `posNeg` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -40,9 +38,9 @@ CREATE TABLE `chores` (
 -- Table structure for table `chorevalues`
 --
 
-CREATE TABLE `chorevalues` (
-  `choreID` int(11) UNSIGNED NOT NULL,
-  `groupID` int(11) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `chorevalues` (
+  `choreID` int(11) unsigned NOT NULL,
+  `groupID` int(11) unsigned NOT NULL,
   `choreValue` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -52,11 +50,12 @@ CREATE TABLE `chorevalues` (
 -- Table structure for table `events`
 --
 
-CREATE TABLE `events` (
-  `eventID` int(11) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `events` (
+  `eventID` int(11) unsigned NOT NULL,
   `reporterUserID` int(11) NOT NULL,
   `choreID` int(11) NOT NULL,
-  `reportedUserID` int(11) NOT NULL
+  `reportedUserID` int(11) NOT NULL,
+  `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -65,9 +64,21 @@ CREATE TABLE `events` (
 -- Table structure for table `groups`
 --
 
-CREATE TABLE `groups` (
-  `groupID` int(11) UNSIGNED NOT NULL,
-  `groupName` varchar(60) NOT NULL
+CREATE TABLE IF NOT EXISTS `groups` (
+  `groupID` int(11) unsigned NOT NULL,
+  `groupName` varchar(60) NOT NULL,
+  `groupCode` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_users`
+--
+
+CREATE TABLE IF NOT EXISTS `group_users` (
+  `groupID` int(11) unsigned NOT NULL,
+  `userID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -76,22 +87,22 @@ CREATE TABLE `groups` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `userID` int(11) UNSIGNED NOT NULL,
-  `username` varchar(30) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `userID` int(11) unsigned NOT NULL,
   `password` varchar(200) NOT NULL,
   `salt` varchar(100) NOT NULL,
   `email` varchar(60) NOT NULL,
   `firstName` varchar(40) NOT NULL,
   `lastName` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `username`, `password`, `salt`, `email`, `firstName`, `lastName`) VALUES
-(1, '', 'f2b4e537e6ebdb2fd7b82218c3f76c820e5e626b96b465497e4590cc1f255285', '9a4b2c2eebffdd6232521c5e61ae313bcab760e6118aa13c7280e689b7521686', 'lipscc@rpi.edu', 'Chris', 'Lipscomb');
+INSERT INTO `users` (`userID`, `password`, `salt`, `email`, `firstName`, `lastName`) VALUES
+(1, 'f2b4e537e6ebdb2fd7b82218c3f76c820e5e626b96b465497e4590cc1f255285', '9a4b2c2eebffdd6232521c5e61ae313bcab760e6118aa13c7280e689b7521686', 'lipscc@rpi.edu', 'Chris', 'Lipscomb'),
+(2, 'ad9999473c8377ad768015e714f5d4f667d88fce973595463da84e3a411f8dd8', '8547001022a44968749a88666e13441e6169a50efde4bdc418f6e70c73382f54', 'risch1997@aol.com', 'Chris', 'Lipscomb');
 
 --
 -- Indexes for dumped tables
@@ -135,27 +146,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `chores`
 --
 ALTER TABLE `chores`
-  MODIFY `choreID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
+  MODIFY `choreID` int(11) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `eventID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
+  MODIFY `eventID` int(11) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `groupID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
+  MODIFY `groupID` int(11) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
+  MODIFY `userID` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
