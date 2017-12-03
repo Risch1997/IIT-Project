de1733b158aa0d0c3d353370769834063dd15ce4
MichaelvMorrison <morrim5@rpi.edu>
2017-12-03 13:44:52 -0500

    Styled index.php

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 <?php include('includes/top.php'); ?>
 <html>
 	<body>
+		<div id="select">
 		<h1>Select an option</h1>
 		<div class="groupoption">
-			<a href="create.php">Create group</a><br/
+			<button class="selectbutton"><a href="create.php">Create group</a><br></button>
 		</div>
 		<div class="groupoption">
-			<a href="join.php">Join group</a><br/
+			<button class="selectbutton"><a href="join.php">Join group</a><br></button>
 		</div>
 		<div class="groupoption">
-			<a href="view.php">View group</a><br/
+			<button class="selectbutton"><a href="view.php">View group</a><br></button>
 		</div>
 	</body>
 </html>
