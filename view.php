bbf6eb0da82351d0822d6db3609575f1eddb9891
MichaelvMorrison <morrim5@rpi.edu>
2017-12-05 13:37:46 -0500

    styled view.php

--- view.php
+++ view.php
@@ -1,28 +1,27 @@
 <?php include('includes/top.php'); ?>
 
-		
-	<h1 style="text-align: center">Your Groups</h1>
+<div id="viewdiv">
+	<h1>Your Groups</h1>
+</div>
 
-	
 	<?php
-		echo '<table class="groupList" style="border:solid; width: 30%">';
+		echo '<table class="grouplisttable">';
 		$query = "SELECT groups.groupName, groups.groupID FROM groups, group_users WHERE ". $_SESSION['userID']." = group_users.userID";
-		
+
 		$result = $dbcon->query($query);
-		
-		
-		
+
+
 		foreach ($result as $row){
 			echo "
 			<tr>
-				<td><label style=\"font-size:20px\">" . $row['groupName'] . "</label></td>
-				<td><a href=\"group.php?id=" . $row['groupID'] . "\"><button type=\"button\"class=\"submit\" style=\"float:right\">View</button></a></td>
-			</tr>";	
+				<td class=\"tdlable\"><label style=\"font-size:20px\">" . $row['groupName'] . "</label></td>
+				<td><a href=\"group.php?id=" . $row['groupID'] . "\"><input type=\"submit\" name=\"joinSubmit\" value=\"View\" class=\"viewsubmit\"></a></td>
+			</tr>";
          }
 		 echo "</table>";
 	?>
-	
+
+</div>
 
 
 <?php include('includes/footer.php'); ?>
-
