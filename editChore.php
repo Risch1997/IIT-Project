84b297d5d5cef206490446141052578c8f7f709a
MichaelvMorrison <morrim5@rpi.edu>
2017-12-06 01:22:42 -0500

    styled editChore.php and started styling group.php

--- editChore.php
+++ editChore.php
@@ -5,17 +5,19 @@
 	if (isset($_GET['choreID']) && isset($_GET['groupID'])){
 		$choreID = $_GET['choreID'];
 		$groupID = $_GET['groupID'];
-		
+
 		if(isset($_POST['editChore'])) {
 			$choreName = isset($_POST['choreName']) ? make_safe($_POST['choreName']) : '';
 			$choreValue = isset($_POST['choreValue']) ? make_safe($_POST['choreValue']) : '';
 			$dbcon->exec("UPDATE `chores` SET `choreName` = '$choreName', `choreValue` = $choreValue WHERE `groupID` = $groupID AND `choreID` = $choreID");
 			header("Location: group.php?id=$groupID");
 		}
-		
+
 		$query = $dbcon->query("SELECT * FROM chores WHERE choreID = $choreID AND groupID = $groupID");
 		$chore = $query->fetch(PDO::FETCH_ASSOC);
 		echo "
+		<div id=\"editchorediv\">
+		<h1>Edit Chore</h1>
 		<form id=\"editChoreForm\" method=\"POST\" action=\"\">
 			<table>
 				<tr>
@@ -28,7 +30,8 @@
 				</tr>
 			</table>
 			<input type=\"submit\" name=\"editChore\" class=\"submit\" value=\"Edit Chore\">
-		</form>";
+		</form>
+		</div>";
 	}
 
 ?>
--- group.php
+++ group.php
@@ -7,19 +7,19 @@ if(isset($_GET['id'])) {
 	$groupID = $_GET['id'];
 	$query = $dbcon->query("SELECT * FROM groups WHERE groupID = $groupID");
 	$group = $query->fetch(PDO::FETCH_ASSOC);
-	
+
 	if(isset($_POST['addChore'])) {
 		$choreName = isset($_POST["choreName"]) ? make_safe($_POST['choreName']) : '';
 		$choreValue = isset($_POST["choreValue"]) ? make_safe($_POST['choreValue']) : '';
 		$dbcon->exec("INSERT INTO `chores` (`groupID`, `choreName`, `choreValue`) VALUES ($groupID, '$choreName', $choreValue)");
 	}
 	if(isset($_POST['reportSubmit'])) {
-		
+
 		$reporterUserID = $_SESSION['userID'];
 		$choreID = isset($_POST["chore"]) ? make_safe($_POST['chore']) : '';
 		$reportedUserID = isset($_POST["reportedUser"]) ? make_safe($_POST['reportedUser']) : '';
 		$dbcon->exec("INSERT INTO `events` (`reporterUserID`, `groupID`, `choreID`, `reportedUserID`) VALUES ($reporterUserID, $groupID, $choreID, $reportedUserID)");
-		
+
 		$query = $dbcon->query("SELECT `choreValue` FROM `chores` WHERE `choreID` = $choreID");
 		$chore = $query->fetch(PDO::FETCH_ASSOC);
 		$query = $dbcon->query("SELECT score FROM `group_users` WHERE `groupID` = $groupID AND `userID` = $reportedUserID");
@@ -28,21 +28,24 @@ if(isset($_GET['id'])) {
 	}
 
 	echo "
-		<div style=\"font-size:30px; margin:10px\">
-			Welcome to the " . $group['groupName'] . " household!
-		</div>";
 
-	echo "
-		<div id=\"household\" style=\"font-size:20px; border-style:solid; height: 100%; width: 29%; margin: 20px; float:right\">
-			<h3 style=\"text-align:center\">Members</h3> <br>
-			<table>";
+		<div id=\"groupwelcomediv\">
+			<h1>Welcome to the " . $group['groupName'] . " household!</h1>
+		</div>
+
+		<div id=\"household\">
+		<div id=\"householddiv\">
+			<h1>Members</h1>
+			</div>
+			<table id=\"householdtable\">
+			</div>";
 
 
 	$query = "SELECT users.firstName, users.lastName,group_users.score FROM users, group_users WHERE group_users.userID = users.userID AND group_users.groupID = " . $groupID;
 	// $result = $dbcon->query($query);
 
 
-	echo "<tr><th>First Name</th><th>Last Name</th><th>Score</th></tr>";
+	echo "<tr><th>First Name</th><th>Last Name</th><th>Score</th><th>View</th></tr>";
 	foreach ($dbcon->query($query) as $result){
 
 		echo "
@@ -50,6 +53,7 @@ if(isset($_GET['id'])) {
 					<td>". $result['firstName'] . "</td>
 					<td>". $result['lastName'] . "</td>
 					<td>". $result['score'] . "</td>
+					<td><input type=\"submit\" name=\"viewProfile\" value=\"View\" class=\"profilesubmit\"></td>
 				</tr>";
 	}
 
@@ -57,25 +61,28 @@ if(isset($_GET['id'])) {
 			</table>
 		</div>";
 
-	echo "
-
-		<div id=\"chores\" style=\"font-size:20px; border-style:solid; width: 29%; margin:20px; float:right;text-align:center\">";
-
 ?>
 
-				<h3>Chores</h3>
-				<table id="choreTable">
+				<div id="choresdiv">
+				<h1>Chores</h1>
+				</div>
+
+
+				<table id="chorestable">
 					<tr>
 						<th>Chore Name</th>
-						<th>Value</th>
+						<th>Chore Value</th>
+						<th>Edit Chore</th>
 					</tr>
+
 				<?php
 					$query = $dbcon->query("SELECT * FROM chores WHERE groupid = $groupID");
 					while($chore = $query->fetch(PDO::FETCH_ASSOC)){
 						echo "
 					<tr>
-						<td><a href=\"event.php?id=" . $chore['choreID'] . "\">". $chore['choreName'] . "</a></td>
-						<td>" . $chore['choreValue'] . "</td>
+						<td class=\"choretdlable\">". $chore['choreName'] . "</a></td>
+						<td class=\"choretdlable\">" . $chore['choreValue'] . "</td>
+						<td><a href=\"editChore.php?choreID=" . $chore['choreID'] . "&groupID=$groupID\"><input type=\"submit\" name=\"editChore\" value=\"Edit\" class=\"editchoresubmit\"></a></td>
 					</tr>";
 					}
 				}
@@ -83,17 +90,18 @@ if(isset($_GET['id'])) {
 				?>
 
 				</table>
-				
-				<h3>Add a Chore</h3>
+
+				<div id="addchorediv">
+				<h1>Add a Chore</h1>
 				<form id="createChore" name="createChore" action="" method="POST">
 					<input type="text" class="form-control" id="chore" name="choreName" placeholder="Chore Name">
 					<input type="text" class="form-control" id="score-val" name="choreValue" placeholder="Score Value">
 					<input type="submit" name="addChore" value="Add Chore" class="submit">
 				</form>
 			</div>
-			
-			<div id="reports">
-				<h3>Report an Event</h3>
+
+			<div id="addeventdiv">
+				<h1>Report an Event</h1>
 				<form id="reportEvent" name="createChore" action="" method="POST">
 					<table>
 						<tr>
@@ -109,7 +117,7 @@ if(isset($_GET['id'])) {
 										<option value=\"" . $chore['choreID'] . "\">" . $chore['choreName'] . "</option>";
 									}
 								?>
-								
+
 								</select>
 							</td>
 						</tr>
@@ -126,14 +134,14 @@ if(isset($_GET['id'])) {
 										<option value=\"" . $user['userID'] . "\">" . $user['firstName'] . " " . $user['lastName'] . "</option>";
 									}
 								?>
-								
+
 								</select>
 							</td>
 						</tr>
 					</table>
 					<input type="submit" name="reportSubmit" value="Report Event" class="submit">
 				</form>
-					
+
 			</div>
 
 
