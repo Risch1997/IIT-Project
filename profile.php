f4d555a9f438ee50a0e24fc6476619aee53dc356
MichaelvMorrison <morrim5@rpi.edu>
2017-12-06 17:23:52 -0500

    styled profile

--- profile.php
+++ profile.php
@@ -8,22 +8,29 @@
 		// load user name
 		$query = $dbcon->query("SELECT firstName, lastName FROM users WHERE userID = $userID");
 		$name = $query->fetch(PDO::FETCH_ASSOC);
-		echo $name['firstName'] . ' ' . $name['lastName'] . '</br>';
-		
+		echo "
+		<div id=\"profilediv\">
+			<h1>" . $name['firstName'] . ' ' . $name['lastName'] . "</h1>";
+
 		// load uesr score
 		$query = $dbcon->query("SELECT score FROM group_users WHERE userID = $userID");
 		$score = $query->fetch(PDO::FETCH_ASSOC);
-		echo $score['score'];
+		echo "<h2> Score = " . $score['score'] . "</h2>
+		</div>
+		<div id=\"profileinfo\">
+		<h2 id=\"h2event\">Events</h2>
+		<ul>";
 
-				
+
 
 		// load the chores user was assigned to
 		$query = "SELECT chores.choreName FROM chores, events WHERE events.reportedUserId = $userID AND events.choreID = chores.choreID AND chores.groupID = $groupID";
 
 		foreach ($dbcon->query($query) as $result){
-			echo $result['choreName'];
+			echo "<li>" . $result['choreName'] . "</Li>";
 		}
 
+		echo "</ul></div>";
 	}
 
 ?>
